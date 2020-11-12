7b14214bdc29ad2ee0e479e97198ace4a29be1f3
mateuszbasinski <mateusz.basinski@valueadd.pl>
2020-11-12 09:53:39 +0100

    build(validation-messages): migrate library to lerna and karma to jest

---
+++ packages/validation-messages/src/lib/components/validation-messages/validation-messages.component.spec.ts
@@ -0,0 +1,35 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormControl, Validators } from "@angular/forms";
+
+import { ValidationMessagesComponent } from "./validation-messages.component";
+import { ValidationMessagesModule } from "../../validation-messages.module";
+
+describe("ValidationMessagesComponent", () => {
+  let component: ValidationMessagesComponent;
+  let fixture: ComponentFixture<ValidationMessagesComponent>;
+  const expectedControl = new FormControl("", [
+    Validators.email,
+    Validators.min(10),
+    Validators.max(10),
+    Validators.maxLength(10),
+    Validators.minLength(3),
+    Validators.required,
+  ]);
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ValidationMessagesModule],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ValidationMessagesComponent);
+    component = fixture.componentInstance;
+    component.control = expectedControl;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ packages/validation-messages/src/lib/validation-messages.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ValidationMessagesComponent } from "./components/validation-messages/validation-messages.component";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ValidationMessagesComponent],
+  exports: [ValidationMessagesComponent],
+})
+export class ValidationMessagesModule {}
