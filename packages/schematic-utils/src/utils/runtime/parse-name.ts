a88c2d3c647938ac698d2f10c9716c5afd3db770
Krzysztof Skorupka <krzysztof.skorupka@valueadd.pl>
2021-07-12 14:45:09 +0200

    feat(schematic-utils): schematic utils from angular schematics

---
+++ packages/schematic-utils/src/utils/runtime/parse-name.ts
@@ -0,0 +1,16 @@
+import { basename, dirname, join, normalize, Path } from "@angular-devkit/core";
+
+export interface Location {
+  name: string;
+  path: Path;
+}
+
+export function parseName(path: string, name: string): Location {
+  const nameWithoutPath = basename(normalize(name));
+  const namePath = dirname(join(normalize(path), name));
+
+  return {
+    name: nameWithoutPath,
+    path: normalize("/" + namePath),
+  };
+}
