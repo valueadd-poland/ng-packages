80c4a6d0e44b35646b869b116c41dedebf0ec833
Krzysztof Skorupka <krzysztof.skorupka@valueadd.pl>
2021-07-12 14:45:09 +0200

    feat(schematic-utils): schematic utils from angular schematics

---
+++ packages/schematic-utils/src/utils/create-app-module.ts
@@ -0,0 +1,28 @@
+import { UnitTestTree } from "@angular-devkit/schematics/testing";
+
+export function createAppModule(
+  tree: UnitTestTree,
+  path?: string
+): UnitTestTree {
+  tree.create(
+    path || "/src/app/app.module.ts",
+    `
+    import { BrowserModule } from '@angular/platform-browser';
+    import { NgModule } from '@angular/core';
+    import { AppComponent } from './app.component';
+    @NgModule({
+    declarations: [
+      AppComponent
+    ],
+    imports: [
+      BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+    })
+    export class AppModule { }
+  `
+  );
+
+  return tree;
+}
